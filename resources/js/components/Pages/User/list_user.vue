270446f3ff51ac425965ebb5610812aeb78b14f2
Masud Rana Tapu <masudranatapu@gmail.com>
2023-12-11 12:19:25 +0600

    done

--- resources/js/components/Pages/User/list_user.vue
+++ resources/js/components/Pages/User/list_user.vue
@@ -7,7 +7,9 @@
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
             <li class="breadcrumb-item active">User</li>
           </ol>
         </div>
@@ -18,29 +20,42 @@
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg-12">
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="card card-primary card-outline">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-              </p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              <table class="table table-bordered">
+                <thead>
+                  <tr>
+                    <th>SL No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Created At</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>1</th>
+                    <td>Masud Rana</td>
+                    <td>masudranatapu@gmail.com</td>
+                    <td>01478523690</td>
+                    <td>Dhaka, Bangladesh</td>
+                    <td>12, Dec 2023</td>
+                    <td>
+                        <div class="btn-group">
+                          <a class="btn btn-primary btn-sm">
+                            Edit
+                          </a>
+                          <a class="btn btn-secondary btn-sm">
+                            Delete
+                          </a>
+                        </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
